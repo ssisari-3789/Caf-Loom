7da30c11856618625ac553164a7e0aad741b9b9e
ssisari-3789 <it20026684@my.sliit.lk>
2024-10-23 11:24:43 +0530

    Created website responsive(specially mobile responsive) using media queries and Made Javascript functions for search and menu

--- script.js
+++ script.js
@@ -2,4 +2,17 @@ let search = document.querySelector(".search-box");
 
 document.querySelector("#search-icon").onclick = () => {
   search.classList.toggle("active");
+  navigation.classList.remove("active");
+};
+
+let navigation = document.querySelector(".navbar");
+
+document.querySelector("#menu-icon").onclick = () => {
+  navigation.classList.toggle("active");
+  search.classList.remove("active");
+};
+
+window.onscroll = () => {
+  navigation.classList.remove("active");
+  search.classList.remove("active");
 };
