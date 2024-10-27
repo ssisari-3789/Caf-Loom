282926a51df0e893b00852c8ec4160cbf907886e
Sisari Kahandawa Kahandawa Arachchi <88578025+ssisari-3789@users.noreply.github.com>
2024-10-28 00:10:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let search = document.querySelector(".search-box");
+let search = document.querySelector(".search-box"); //create a function for hide the navigation bar when click the search icon
 
 document.querySelector("#search-icon").onclick = () => {
   search.classList.toggle("active");
   navigation.classList.remove("active");
 };
 
-let navigation = document.querySelector(".navbar");
+let navigation = document.querySelector(".navbar");  //create a function for hide the search box when click the menu icon
 
 document.querySelector("#menu-icon").onclick = () => {
   navigation.classList.toggle("active");
